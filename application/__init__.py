b128b50a30f8fccffdfbd4b79374737dafd76d87
Golden TM <mumanikidzwag@gmail.com>
2019-12-06 18:13:05 +0800

    added admin dashboard

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = '12345rtyh7u89ko0pjgtt54fg34568u3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///EAS.db'
 
 db = SQLAlchemy(app)
--- application/models.py
+++ application/models.py
@@ -7,36 +7,32 @@ class Admin(db.Model):
     username = db.Column(db.String(10), nullable=False, unique=True)
     password = db.Column(db.String(30), nullable=False)
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(30), nullable=False)
-    studentnumber = db.Column(db.String(20), nullable=False, unique=True)
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column(db.String(20), primary_key=True)
+    firstname = db.Column(db.String(30), nullable=False)
+    lastname = db.Column(db.String(30), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
     gender = db.Column(db.String(5), nullable=False)
     nationality = db.Column(db.String(15), nullable=False)
     email = db.Column(db.String(20), nullable=False, unique=True)
     mobile = db.Column(db.String(20), nullable=False, unique=True)
-    enrolleddate = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     password = db.Column(db.String(30), nullable=False)
+    courses = db.relationship('Course', backref='teacher')
+
+class SelectedCourses(db.Model):
+    __tablename__ = 'selected_courses'
+    student_id = db.Column(db.String(20), db.ForeignKey('user.user_id'), primary_key=True)
+    course_number = db.Column(db.String(10), db.ForeignKey('course.coursenumber'), primary_key=True)
 
 class Course(db.Model):
+    __tablename__ = 'course'
     id = db.Column(db.Integer, primary_key=True)
     coursenumber = db.Column(db.String(10), nullable=False, unique=True)
     coursename = db.Column(db.String(15), nullable=False)
     time = db.Column(db.String(20), nullable=False)
     semester = db.Column(db.String(10), nullable=False)
     credit = db.Column(db.String(5), nullable=False)
-    gpa = db.Column(db.String(5), nullable=False)
     location = db.Column(db.String(10), nullable=False)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'), nullable=False)
+    teacher_id = db.Column(db.String(20), db.ForeignKey('user.user_id'), nullable=True)
 
-class Teacher(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    tid = db.Column(db.String(15), nullable=False, unique=True)
-    fullname = db.Column(db.String(30), nullable=False)
-    title = db.Column(db.String(15), nullable=False)
-    gender = db.Column(db.String(5), nullable=False)
-    nationality = db.Column(db.String(15), nullable=False)
-    email = db.Column(db.String(20), nullable=False, unique=True)
-    mobile = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(30), nullable=False)
-    hire_on = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
