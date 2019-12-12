b46632788a62b6bc952b486b4c265bf58639c2b9
Golden TM <mumanikidzwag@gmail.com>
2019-12-12 16:54:35 +0800

    fixing login authentication error

--- application/forms.py
+++ application/forms.py
@@ -6,6 +6,14 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 class LoginForm(FlaskForm):
 
     id = StringField('ID', validators=[DataRequired(), Length(min=3, max=15)])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=16)])
+
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
+
+class AdminLoginForm(FlaskForm):
+
     username = StringField('User Name', validators=[DataRequired(), Length(min=3, max=10)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=16)])
 
