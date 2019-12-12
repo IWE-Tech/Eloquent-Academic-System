34a5c007835756359e7bf8902ce552c16a26a6a4
abdi <abdullahix2008@gmail.com>
2019-12-12 13:37:26 +0800

    added current_user functionality

--- application/routes.py
+++ application/routes.py
@@ -14,9 +14,8 @@ def home():
 
 @app.route('/MyCourses')
 def my_courses():
-    student = User.query.filter_by(user_id=current_user.user_id).first()
-    my_courses = Course.query.join(SelectedCourses, Course.coursenumber == SelectedCourses.course_number).filter_by(student_id=student.user_id).all()
-    return render_template('student/my_courses.html', my_courses=my_courses, student=student,
+    my_courses = Course.query.join(SelectedCourses, Course.coursenumber == SelectedCourses.course_number).filter_by(student_id=current_user.user_id).all()
+    return render_template('student/my_courses.html', my_courses=my_courses,
     SelectedCourses=SelectedCourses, func=func, db=db)
 
 @app.route('/course_selection', methods=['GET', 'POST'])
@@ -26,8 +25,7 @@ def courses_selection():
 
 @app.route('/Course/<string:course_number>/Select', methods=['GET', 'POST'])
 def select_course(course_number):
-    student = User.query.filter_by(user_id=current_user.user_id).first()
-    result = SelectedCourses.query.filter(and_(SelectedCourses.student_id == student.user_id,
+    result = SelectedCourses.query.filter(and_(SelectedCourses.student_id == current_user.user_id,
     SelectedCourses.course_number==course_number)).first()
     
     if result:
@@ -35,20 +33,19 @@ def select_course(course_number):
         return redirect(url_for('courses_selection'))
     else:
         this_course = Course.query.filter_by(coursenumber=course_number).first()
-        course = SelectedCourses(student_id=student.user_id,
+        course = SelectedCourses(student_id=current_user.user_id,
         course_number=course_number, course_time=this_course.time,
         course_credit=this_course.credit)
         #                                  ***
         # we have to check if this student have or not a class at the time of this course before he/she select
-        c_with_st = SelectedCourses.query.filter(and_(SelectedCourses.student_id == student.user_id,
+        c_with_st = SelectedCourses.query.filter(and_(SelectedCourses.student_id == current_user.user_id,
         SelectedCourses.course_time == this_course.time)).first()
         total_credit = db.session.query(func.sum(SelectedCourses.course_credit)).filter(SelectedCourses.student_id ==
-        student.user_id).scalar()
-       
-
-        # if int(total_credit) + int(this_course.credit) > 25:
-        #     flash('You have reached maximum semester credits!', 'warning')
-        #     return redirect(url_for('courses_selection'))
+        current_user.user_id).scalar()
+        student_courses = SelectedCourses.query.filter_by(student_id=current_user.user_id).all()
+        if student_courses and int(total_credit) + int(this_course.credit) > 25:
+            flash('You have reached maximum semester credits!', 'warning')
+            return redirect(url_for('courses_selection'))
         if c_with_st:
             flash('You have another class at this time!', 'danger')
             return redirect(url_for('courses_selection'))
@@ -60,8 +57,7 @@ def select_course(course_number):
 
 @app.route('/Course/<string:course_number>/drop', methods=['GET', 'POST'])
 def drop_course(course_number):
-    student = User.query.filter_by(user_id=current_user.user_id).first()
-    result = SelectedCourses.query.filter(and_(SelectedCourses.student_id == student.user_id,
+    result = SelectedCourses.query.filter(and_(SelectedCourses.student_id == current_user.user_id,
     SelectedCourses.course_number==course_number)).first()
     if result:
         db.session.delete(result)
@@ -78,15 +74,18 @@ def login():
     teacher = 'teacher'
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(user_id=form.id.data).first()                   #query the user from the database
-        if user.user_id == form.id.data and user.password == form.password.data:  
-            if user.role == teacher:                    #if the user's role is teacher
+        user = User.query.filter_by(user_id=form.id.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):  
+            if user.role == 'teacher':                    #if the user's role is teacher
                 login_user(user)
                 return redirect(url_for('teacherHome', teacher_name=(user.firstname + user.lastname)))
 
-            elif user.role == student:                  #if the user's role is student
+            elif user.role == 'student':                  #if the user's role is student
                 login_user(user)
                 return redirect(url_for('my_courses'))
+            else:
+                flash('Unknown error occured')
+                return redirect(url_for('home'))
         else:
             flash('Login failed, please check your id and password', 'danger')
 
@@ -135,13 +134,13 @@ def add_user():
     password = request.form.get("password")
 
     if id and first and last and role and gender and nationality and email and mobile and password:
-        #hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
         check_user = User.query.filter_by(user_id=id).first()
         if check_user:
             flash('The User is already registered in the system!', 'danger')
             return redirect(url_for('add_user'))
         else:
-            user = User(user_id=id, firstname=first, lastname=last, role=role, gender=gender, nationality=nationality, email=email, mobile=mobile, password=password)
+            user = User(user_id=id, firstname=first, lastname=last, role=role, gender=gender, nationality=nationality, email=email, mobile=mobile, password=hashed_password)
             db.session.add(user)
             db.session.commit()
             flash('The new user has been added successfully', 'success')
